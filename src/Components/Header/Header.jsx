1187c03eeb56cb90bc442b7672ebf92aaab86006
Lineles <106559385+Lineles@users.noreply.github.com>
2022-09-27 13:09:48 +0200

    created MainPage .jsx/.css (basic framework for Page)

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+
+
+import "./Header.css"
+
+function Header () {
+
+
+    return(
+
+        <div className="header-flex">
+            <button className="button-header">!!! BUTTONS !!!</button>
+
+        </div>
+    )
+}; 
+
+export default Header; 
+
---
+++ src/Components/MainPage/MainPage.jsx
@@ -0,0 +1,21 @@
+
+
+import "./MainPage.css"
+import Header from "../Header/Header";
+
+
+function MainPage () {
+
+
+    return (
+
+        <div> 
+            <Header />
+            <div className="MainPage-Panels-Flex">
+
+            </div>
+        </div>
+    )
+}; 
+
+export default MainPage; 
